f320302282f046afd6e6b299dc209d5317fe057f
Serhat Obay <serhat.obay2121@gmail.com>
2022-12-01 21:16:05 +0300

    Tr Map Bug Fixed

--- resources/views/europa_table.blade.php
+++ resources/views/europa_table.blade.php
@@ -4,59 +4,59 @@
     <a type="button" href="{{ url('/') }}" class="btn btn-outline-info" >ANA SAYFAYA GERİ DÖN</a>
     <v
 
-        <div class="container-fluid">
+    <div class="container-fluid">
 
-            <div class="row">
+        <div class="row">
 
-                <div class="col-sm-14 col-md-15 col-lg-12 col-lg-offset-15 main">
-                    <br>
-                    <h1 class="page-header" style="color: white">Avrupa Ülkeleri</h1>
+            <div class="col-sm-14 col-md-15 col-lg-12 col-lg-offset-15 main">
+                <br>
+                <h1 class="page-header" style="color: white">Avrupa Ülkeleri</h1>
 
-                    <div class="table-responsive">
-                        <table class="table table-hover table-bordered">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th>Ülke Adı</th>
-                                <th>Nüfus</th>
-                                <th>Yüz Ölçümü</th>
-                                <th>Bayrak</th>
-                                <th>Şehir Sayısı</th>
-                                <th>Domain</th>
-                                <th>Alan Kodu</th>
-                                <th>Resmi Dili</th>
-                                <th>Para Birimi</th>
-                                <th>Kuruluş Yılı</th>
-                                <th>Detaylı Bilgi</th>
-                            </tr>
-                            </thead>
-                            <tbody>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th>Ülke Adı</th>
+                            <th>Nüfus</th>
+                            <th>Yüz Ölçümü</th>
+                            <th>Bayrak</th>
+                            <th>Şehir Sayısı</th>
+                            <th>Domain</th>
+                            <th>Alan Kodu</th>
+                            <th>Resmi Dili</th>
+                            <th>Para Birimi</th>
+                            <th>Kuruluş Yılı</th>
+                            <th>Detaylı Bilgi</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td style="width: 100px">
-                                        <a href="" class="btn btn-xs btn-success" data-toggle="tooltip" data-placement="top" title="Düzenle">
-                                            <span class="fa fa-pencil"></span>
-                                        </a>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td style="width: 100px">
+                                <a href="" class="btn btn-xs btn-success" data-toggle="tooltip" data-placement="top" title="Düzenle">
+                                    <span class="fa fa-pencil"></span>
+                                </a>
 
-                                    </td>
-                                </tr>
+                            </td>
+                        </tr>
 
-                            </tbody>
+                        </tbody>
 
-                        </table>
-                    </div>
+                    </table>
                 </div>
             </div>
         </div>
+    </div>
 
 
 
